ddf5ce8e84275d3b56f66571cd67139ad53b25d5
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2026-03-05 22:13:16 -0500

    Update Information

--- resources/ts/navigation/vertical/index.ts
+++ resources/ts/navigation/vertical/index.ts
@@ -18,7 +18,7 @@ const menu: NavItem[] = [
     icon: { icon: 'tabler-building-broadcast-tower' },
     roles: ['admin', 'operador'],    
     children: [
-      { title: 'Documentos Electrónicos - Dian- 2025 2026 123 ok',to:{name:'documentosdian'}, icon: { icon: 'tabler-clipboard-text', size: 18 }, roles: ['admin', 'operador',]},
+      { title: 'Documentos Electrónicos - Dian- 2025 2026 123 ok LAB',to:{name:'documentosdian'}, icon: { icon: 'tabler-clipboard-text', size: 18 }, roles: ['admin', 'operador',]},
       { title: 'Notas Electrónicas', to: { name: 'documentosdian-notes' }, icon: { icon: 'tabler-brand-notion', size: 18 }, roles: ['admin'] },
       { title: 'Nómina Electrónica', to: { name: 'documentosdian-payroll' }, icon: { icon: 'tabler-user-circle', size: 18 }, roles: ['admin'] },
       { title: 'Documento Soporte', to: { name: 'documentosdian-support' }, icon: { icon: 'tabler-file-invoice', size: 18 }, roles: ['admin'] },
