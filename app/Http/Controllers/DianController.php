98b7a67ca3d757f7e908a0abf71e5e66b2661f02
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2026-03-28 19:03:05 -0500

    Update ControlToken

--- app/Http/Controllers/DianController.php
+++ app/Http/Controllers/DianController.php
@@ -184,7 +184,7 @@ class DianController extends Controller
                 'nit_dian'      => Auth::user()->company->nit,
                 'user_id'       => Auth::user()->id,
                 'codigointerno' => Auth::user()->company->codigointerno ?? '',
-                'hora' => now()->toDateTimeString(),
+                'Fecha' => now(),
             ]);
 
         if ($response->successful()) {
