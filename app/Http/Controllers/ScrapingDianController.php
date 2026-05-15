71cb0246f2b44a99571156f460275bd7459cc0d0
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2026-05-15 10:37:55 -0500

    Update Detalles Ventas

--- app/Http/Controllers/ScrapingDianController.php
+++ app/Http/Controllers/ScrapingDianController.php
@@ -361,6 +361,102 @@ class ScrapingDianController extends Controller
         return response()->json(['error' => 'Error al conectar con el servicio externo'], 500);
     }
 
+    public function extraerTabla_detallesventas(Request $request)
+    {
+        $url = $request->url_token;
+        $fechadesde = $request->fechadesde;
+        $fechahasta = $request->fechahasta;
+        $codigointerno =  Auth::user()->code_n8n;
+        //$url = "https://catalogo-vpfe.dian.gov.co/User/AuthToken?pk=10910094|77193886&rk=901148547&token=abe167ee-c7c5-4cba-be6b-0f28ff25679d";
+        $ventas = SalesInvoice::select('id', 'date_issue', 'cufe')
+            ->whereBetween('date_issue', [$fechadesde, $fechahasta])->get();
+
+        // return response()->json([
+        //     'status'          => 'success',
+        //     'fechadesde'      => $fechadesde,
+        //     'fechahasta'      => $fechahasta,
+        //     'url'            => $url,
+        // ], 200);
+
+        $company = Company::find($request->company_id);
+        $endpoint = "http://194.163.159.64:8000/descargar-dian";
+        $endpoint = preg_replace('/\\s+/', '', $endpoint);
+
+        $urlDian = html_entity_decode($url, ENT_QUOTES, 'UTF-8');
+
+        try {
+            $response = Http::withoutVerifying()
+                ->timeout(180)
+                ->connectTimeout(10)
+                ->withHeaders([
+                    'X-API-KEY'    => '6ed6d9ae8423598a5287ab60df52442f1d60c3ae5fcf877bcdbc1fedd1d24316',
+                    'Content-Type' => 'application/json; charset=UTF-8',
+                    'Accept'       => 'application/json',
+                ])->post($endpoint, [
+                    'nitempresa'             => $company->nit,
+                    'nitrepresentantelegal'  => $company->nit_representante_legal,
+                    'fechadesde'             => $request->fechadesde,
+                    'fechahasta'             => $request->fechahasta,
+                    'type'                   => "1",
+                    'headless'               => true,
+                    'url_dian'               => $urlDian,
+                    'datos_cufe'             => $ventas,
+                    'codigointerno'          => $codigointerno,
+                    'companies_id'           => $request->company_id,
+                ]);
+        } catch (\Illuminate\Http\Client\ConnectionException $e) {
+            return response()->json([
+                'message' => 'Error al consumir la API',
+                'error' => $e->getMessage()
+            ], 408);
+        }
+
+
+        if ($response->successful()) {
+            $rawRecords = collect($response->json('data') ?? []);
+
+            $filteredData = $rawRecords
+                // 1. Filtrar solo los tipos de documento permitidos
+                ->filter(function ($item) {
+                    return in_array($item['TipoDocumento'], ['Factura electrónica', 'Nota de crédito electrónica']);
+                })
+                // 2. Mapear y transformar valores
+                ->map(function ($item) {
+                    $esNotaCredito = ($item['TipoDocumento'] === 'Nota de crédito electrónica');
+
+                    // Campos a convertir a numérico
+                    $camposNumericos = ['ValorTotal', 'ValorImptos', 'ValorRetefuente', 'ValorReteiva', 'ValorReteica'];
+
+                    foreach ($camposNumericos as $campo) {
+                        $valor = floatval($item[$campo] ?? 0);
+                        // Si es Nota de Crédito, multiplicamos por -1
+                        $item[$campo] = $esNotaCredito ? ($valor * -1) : $valor;
+                    }
+
+                    return $item;
+                })
+                // 3. Ordenar por Fecha (de la más antigua a la más reciente)
+                ->sortBy('Fecha')
+                ->values(); // Resetear índices del array
+
+            try {
+                $this->updatePurchasesInvoices($filteredData, $request->company_id);
+            } catch (\Throwable $e) {
+                dd($e->getMessage(), $e->getLine(), $e->getFile());
+            }
+
+            return response()->json([
+                'status'          => 'success',
+                'type'            => $request->type,
+                'TotalDocumentos' => $filteredData->count(),
+                'TotalValor'      => $filteredData->sum('ValorTotal'),
+                'TotalIva'        => $filteredData->sum('ValorImptos'),
+                'data'            => $filteredData,
+            ], 200);
+        }
+
+        return response()->json(['error' => 'Error al conectar con el servicio externo'], 500);
+    }
 
 
     public function updateSales($resp, $company_id)
